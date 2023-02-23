9c68e3fabf06087bd87676187e83f9baf63aa142
DevIA05 <dia05@polenumerique.re>
2023-02-23 23:14:01 +0400

    ajout d'un endpoint: prédiction

--- src/model/urls.py
+++ src/model/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('model', views.page_model, name='page_model'),
+    re_path(r'^makesThePrediction$', views.makesThePrediction),
 ]

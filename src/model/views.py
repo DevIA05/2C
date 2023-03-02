49c1c34fd18ed62fd362a36d360aba90b9c2d10d
DevIA05 <dia05@polenumerique.re>
2023-03-02 13:31:34 +0400

    ajout des tables dans postgresql + conversion d'une image en base64 avant de l'envoyer dans le template

--- src/model/views.py
+++ src/model/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import pdb; #pdb.set_trace()
 from model.models import MultipleImage
 from django.http import JsonResponse
 import base64
-import io
+import io, os
 from PIL import Image
 import random
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import re
 
 # from django.conf.settings import PATH_IMG_INVALIDED, PATH_IMG_POSSESSED
 from django.conf import settings
@@ -13,18 +15,25 @@ from django.conf import settings
 # Create your views here.
 
 def page_model(request):
-    # images = MultipleImage.objects.all()
-    #print(settings.PATH_IMG_INVALIDED)
-    return render(request, 'model.html')
+    path_images = MultipleImage.objects.values_list('pathimg', flat=True)
+    images = []
+    for path in path_images:
+        file_name = re.search(r'[^\\]+$', path).group()
+        images.append(image_to_base64(path))
+    return render(request, 'model.html', {'images': images})
 
 
 def makesThePrediction(request):
-    pdb.set_trace()
     imgB64 = request.POST.get('result[image]')
     imgname = request.POST.get('result[name]')
     nombre_aleatoire = random.uniform(0.5, 1)
     nombre_arrondi = round(nombre_aleatoire, 2)
     return JsonResponse({"name": imgname,  "acc": nombre_arrondi})
+
+def image_to_base64(path):
+    with open(path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    return encoded_string
 
 def base64_to_image(base64_string):
     # Decode the Base64 string to bytes
@@ -33,3 +42,4 @@ def base64_to_image(base64_string):
     image_buffer = io.BytesIO(image_bytes)
     # Open the image with PIL and return it
     return Image.open(image_buffer)
+

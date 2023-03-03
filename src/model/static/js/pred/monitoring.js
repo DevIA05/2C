7c69e05fac86a8de4529f283a2a1c294d5812a19
DevIA.E13 <DevIA.E13@afpar.com>
2023-03-03 10:50:21 +0400

    Update monitoring.js
    Update recup_data() : Récupère toute les donnée du modal et envoie les données avec la methode fetch()

--- src/model/static/js/pred/monitoring.js
+++ src/model/static/js/pred/monitoring.js
@@ -1,10 +1,11 @@
-function date() {
+function date_() {
     const now = new Date();
     const day = ('0' + now.getDate()).slice(-2); // ajoute un zéro devant si nécessaire et prend les deux derniers caractères
     const month = ('0' + (now.getMonth() + 1)).slice(-2); // ajoute un zéro devant si nécessaire et prend les deux derniers caractères
     const year = now.getFullYear();
     const formattedDate = day + '/' + month + '/' + year;
-    console.log(formattedDate); // affiche la date sous la forme JJ/MM/AAAA
+    // console.log(formattedDate); // affiche la date sous la forme JJ/MM/AAAA
+    return formattedDate
 };
 
 function heure_h24() {
@@ -13,23 +14,56 @@ function heure_h24() {
     const minutes = ('0' + now.getMinutes()).slice(-2); // ajoute un zéro devant si nécessaire et prend les deux derniers caractères
     const seconds = ('0' + now.getSeconds()).slice(-2); // ajoute un zéro devant si nécessaire et prend les deux derniers caractères
     const formattedTime = hours + ':' + minutes + ':' + seconds;
-    console.log(formattedTime); // affiche l'heure sous format 24h (ex: 14:23:58)
+    // console.log(formattedTime); // affiche l'heure sous format 24h (ex: 14:23:58)
+    return formattedTime
 };
 
+
+
+
 function recup_data(button, i) {
-    const stock_data = {date : "date", cat_modele : "cat_modele"};
+    const stock_data = {};
     const regex2 = /\s(\S+)$/;
     let res_label = document.getElementById("btn_cat_id" + i);
     let res_model = document.getElementById("btn_id" + i);
+    let res_pred = document.getElementById("pred_id" + i);
+    let res_img = document.getElementById("img_id" + i);
+    let res_label_model = document.getElementById("label_id" + i);
+    const  Heure = heure_h24();
+    const date = date_();
     if (res_label.textContent !== null & res_model.textContent !== null) {
       res_label = res_label.textContent.match(regex2)[1].trim();
       res_model= res_model.textContent.match(regex2)[1].trim();
+      res_pred = res_pred.textContent;
+      res_img = res_img.src;
+      res_label_model = res_label_model.textContent;
+    };
+    stock_data["lblbyuser"] = res_label;
+    stock_data["ctgbyuser"] = res_model;
+    stock_data["imgB64"] = res_img;
+    stock_data["accuracy"] = res_pred;
+    stock_data["date"] = date;
+    stock_data["heure"] = Heure;
+    stock_data["ctgbymodel"] = res_label_model;
+
+    const url = 'https://example.com/api/my-endpoint/'; // URL de votre endpoint Django
+    const csrftoken  = $('input[name="csrfmiddlewaretoken"]').val()   // collect token
+    
+    const options = {
+      method: 'POST', 
+      headers: { 
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken // Inclut le jeton CSRF dans l'en-tête de la requête
+      }, 
+      body: JSON.stringify(stock_data),
     };
     
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+
   }
-//   const now = new Date();
-//   const hours = now.getHours();
-//   console.log(now)
 
 
 
